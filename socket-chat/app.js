e232c5d26f064b3d012c3678dc49323c040780d8
Anjana <anjanav@coli.uni-saarland.de>
2015-11-29 22:11:02 -0500

    Handle log off event, get rid of global usernames

--- socket-chat/app.js
+++ socket-chat/app.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var usernames = {};
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -10,16 +9,15 @@ app.get('/', function (req, res) {
 io.on('connection', function(socket){
   console.log('CONNECT', socket.id);
   socket.on('log on', function(name){
-      console.log('LOG ON ' + name);
-      usernames[socket.id] = name;
-      io.emit('log on', name);
+    console.log('LOG ON', name);
+    io.emit('log on', name);
+  })
+  socket.on('log off', function(name) {
+    console.log('LOG OFF', name);
+    socket.broadcast.emit('log off', name);
   })
   socket.on('disconnect', function(){
     console.log('DISCONNECT', socket.id);
-    socket.broadcast.emit('log off', usernames[socket.id]);
-    if (socket.id in usernames) {
-        delete usernames[socket.id];
-    }
   });
   socket.on('chat message', function(msg){
     console.log('MESSAGE', msg.username, msg.message);
