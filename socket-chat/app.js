02c680d15265de991b894e2b7576886ddb4074c1
Anjana <anjanav@coli.uni-saarland.de>
2015-11-29 19:31:46 -0500

    Start chat app

---
+++ socket-chat/app.js
@@ -0,0 +1,11 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
