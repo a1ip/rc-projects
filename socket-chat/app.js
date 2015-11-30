97ad05a2b1c373e1433878643988e98222e0b421
Anjana <anjanav@coli.uni-saarland.de>
2015-11-29 19:49:06 -0500

    Handle basic connect/disconnect

--- socket-chat/app.js
+++ socket-chat/app.js
@@ -1,11 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
-app.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+var io = require('socket.io')(http);
 
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
