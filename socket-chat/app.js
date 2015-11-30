520740f0f268e81d8adfd4b490232415bed850af
Anjana <anjanav@coli.uni-saarland.de>
2015-11-29 21:15:43 -0500

    Add usernames

--- socket-chat/app.js
+++ socket-chat/app.js
@@ -1,19 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var usernames = {};
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('CONNECT', socket.id);
+  socket.on('log on', function(name){
+      console.log('LOG ON ' + name);
+      usernames[socket.id] = name;
+      io.emit('log on', name);
+  })
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('DISCONNECT', socket.id);
+    socket.broadcast.emit('log off', usernames[socket.id]);
+    if (socket.id in usernames) {
+        delete usernames[socket.id];
+    }
   });
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    console.log('MESSAGE', msg.username, msg.message);
     io.emit('chat message', msg);
   });
 });
